e6f633b0725666d056b82b959a9014b04a036cf6
Skjeld <a.dreake@yandex.ru>
2025-03-26 21:02:21 +0300

    First commit

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import './assets/main.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.scss'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import Menubar from 'primevue/menubar'
+import Aura from '@primeuix/themes/aura'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      prefix: 'p',
+      darkModeSelector: 'system',
+      cssLayer: false,
+    },
+  },
+})
+app.component('app-menubar', Menubar)
+
+app.mount('#app')
