59014bf2b2d8b4edba299aa6e9912c28b4b356bd
Skjeld <a.dreake@yandex.ru>
2025-03-27 21:51:17 +0300

    Выход из профиля

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,8 +2,11 @@
 import { useUserStore } from '@/stores/user.ts'
 import { ref, computed } from 'vue'
 import type { ComputedRef } from 'vue'
+import { getAuth, signOut } from 'firebase/auth'
+import { useRouter } from 'vue-router'
 
 const userStore = useUserStore()
+const router = useRouter()
 
 interface IMenuItem {
   label: string
@@ -38,6 +41,11 @@ const items = ref<IMenuItem[]>([
     show: computed(() => !!userStore.userId),
   },
 ])
+
+const signOutMethod = async (): Promise<void> => {
+  await signOut(getAuth())
+  router.push('/auth')
+}
 </script>
 
 <template>
@@ -53,7 +61,7 @@ const items = ref<IMenuItem[]>([
     <template #end>
       <span
         v-if="userStore.userId"
-        @click="userStore.userId = ''"
+        @click="signOutMethod"
         class="flex align-items-center menu-exit"
       >
         <span class="pi pi-sign-out p-menuitem-icon" />
