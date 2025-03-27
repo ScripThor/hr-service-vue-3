ffc91022b0f739e2b4c231b41565a3493dba6e15
Skjeld <a.dreake@yandex.ru>
2025-03-27 21:46:50 +0300

    Авторизация и регистрация пользователя

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -2,6 +2,6 @@ import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useUserStore = defineStore('user', () => {
-  const userId = ref<string>('12')
+  const userId = ref<string>('')
   return { userId }
 })
